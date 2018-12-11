38804b7a3d31b11e7f8fe269953054160839b8d5
Shawn Oakley <shawn.oakley@youvisit.com>
2018-12-10 22:22:47 -0500

    Adds example code for double buffer

--- DoubleBuffer.cc
+++ DoubleBuffer.cc
@@ -10,3 +10,79 @@
 // When our rendering code is done drawing the scene, it swaps the buffers. 
 // This tells the video hardware to start reading from the second buffer now instead of the first one. 
 // As long as it times that switch at the end of a refresh, we won’t get any tearing, and the entire scene will appear all at once.
+
+// When to use it:
+// 1) We have some state that is being modified incrementally.
+
+// 2) That same state may be accessed in the middle of modification.
+
+// 3) We want to prevent the code that’s accessing the state from seeing the work in progress.
+
+// 4) We want to be able to read the state and we don’t want to have to wait while it’s being written.
+
+
+// Example Code:
+class Framebuffer
+{
+public:
+  Framebuffer() { clear(); }
+
+  void clear()
+  {
+    for (int i = 0; i < WIDTH * HEIGHT; i++)
+    {
+      pixels_[i] = WHITE;
+    }
+  }
+
+  void draw(int x, int y)
+  {
+    pixels_[(WIDTH * y) + x] = BLACK;
+  }
+
+  const char* getPixels()
+  {
+    return pixels_;
+  }
+
+private:
+  static const int WIDTH = 160;
+  static const int HEIGHT = 120;
+
+  char pixels_[WIDTH * HEIGHT];
+};
+
+class Scene
+{
+public:
+  Scene()
+  : current_(&buffers_[0]),
+    next_(&buffers_[1])
+  {}
+
+  void draw()
+  {
+    next_->clear();
+
+    next_->draw(1, 1);
+    // ...
+    next_->draw(4, 3);
+
+    swap();
+  }
+
+  Framebuffer& getBuffer() { return *current_; }
+
+private:
+  void swap()
+  {
+    // Just switch the pointers.
+    Framebuffer* temp = current_;
+    current_ = next_;
+    next_ = temp;
+  }
+
+  Framebuffer  buffers_[2];
+  Framebuffer* current_;
+  Framebuffer* next_;
+};
