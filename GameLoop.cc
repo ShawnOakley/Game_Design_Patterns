198a8dea2ca327e740b63ba095b3d5ca905050e5
Shawn Oakley <shawn.oakley@youvisit.com>
2018-12-10 23:01:59 -0500

    Adds game loop

---
+++ GameLoop.cc
@@ -0,0 +1,9 @@
+// Definition
+// A game loop runs continuously during gameplay. 
+// Each turn of the loop, it processes user input without blocking, updates the game state, and renders the game. 
+// It tracks the passage of time to control the rate of gameplay.
+
+
+// The difference between an “engine” and a “library”: 
+// With libraries, you own the main game loop and call into the library. 
+// An engine owns the loop and calls into your code.
