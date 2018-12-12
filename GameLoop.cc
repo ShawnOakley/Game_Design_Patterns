e3b05cadef2eec2c5aec4218ecfb2bf0754ec8be
Shawn Oakley <shawn.oakley@youvisit.com>
2018-12-11 21:01:30 -0500

    Adds notes on gameloop

--- GameLoop.cc
+++ GameLoop.cc
@@ -7,3 +7,7 @@
 // The difference between an “engine” and a “library”: 
 // With libraries, you own the main game loop and call into the library. 
 // An engine owns the loop and calls into your code.
+
+// variable or fluid time step:
+// choose a time step to advance based on how much real time passed since the last frame. 
+// The longer the frame takes, the bigger steps the game takes. It always keeps up with real time because it will take bigger and bigger steps to get there. 
