d17d5b71188ea9099ecfb41938cdeb1dd36bf763
Shawn Oakley <shawn.oakley@youvisit.com>
2018-12-18 22:25:16 -0500

    Adds definition for update functionality

--- UpdateMethod.cc
+++ UpdateMethod.cc
@@ -1,2 +1,5 @@
 // Update Method
-// Definition
+// Definition:
+    // The game world maintains a collection of objects. 
+    // Each object implements an update method that simulates one frame of the object’s behavior. 
+    // Each frame, the game updates every object in the collection.
